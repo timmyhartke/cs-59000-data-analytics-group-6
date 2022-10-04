4d3a7e111f1ed4bb5d243e938756a5e7f3888e92
unknown <timmyhartke@gmail.com>
2022-10-04 16:13:27 -0400

    Update main.r to contain code for narrowing down data columns

--- main.r
+++ main.r
@@ -1,7 +1,5 @@
 library(tidyverse)
 library(readxl)
-library(glmnet)
-
 #-----------------
 #READ DATA FROM FILE
 #-----------------
@@ -13,55 +11,37 @@ dataset <- read_excel("D_FINAL.xlsx")
 # This line removes all unnecessary columns. This is a very large dataset, 
 # so we have many columns to remove. We are removing columns by index to 
 # avoid having an impractically large command
-correct_dataset_columns <- dataset[-c(1, 3:11, 15:16, 18, 20, 22, 25, 35, 38, 
-                                      41:49, 52, 58:62, 65, 67:70,73:87, 
-                                      90, 94,96:98, 101, 102, 104:106, 107, 113, 
-                                      118, 119, 124, 142:160, 167:180)]
+correct_dataset_columns <- dataset[c(14, 23, 24, 27:32, 34, 40, 50, 63, 
+                                     92, 100,103, 111,120, 136, 137)]
 
 
 
 # There are a lot of nasty column names such as 
-# 'Congestion rank (INRIX) [dimensionless]' that should be renamed to things like 
+# 'Congestion rank (INRIX) [dimensionless]' that should be renamed to things like   
 # 'congestion rank' instead. This renames each of those column names to something
 # easier to work with
-colnames(correct_dataset_columns) <- c('city name', 'study year', 'reporting year',
-                                       'scope 1 emissions', 'year of emission',
-                                       'gases included', 'increase/decrease',
-                                       'scope 2 emissions','total emissions',
-                                       'emissions quality flag', 's1 lower bound',
-                                       's1 upper bound', 's1 mean', 'tot lower bound',
-                                       'tot upper bound', 'tot mean', 'scope fraction',
-                                       'co2 emissions per capita', 'latitude',
-                                       'longitude', 'country', 'region', 'population',
-                                       'population year', 'population 1950', 
-                                       'population 1990', 'population 2010',
-                                       'population growth rate 1950-2010',
-                                       'population growth rate 1990-2010', 
-                                       'city area km', 'city area year', 
-                                       'population density', 'average altitude',
-                                       'average annual temperature celcius',
-                                       'gdp-ppp', 'gdp-ppp per capita', 
-                                       'gdp-ppp year', 'ngdp', 'ngdp per capita',
-                                       'ngdp-year', 'total consumption per capita',
-                                       'energy per person co2', 'center of commerce index',
-                                       'urbanization ratio', 'water bounded',
-                                       'other bounded', 'mean one-way travel time',
-                                       'mean one-way travel time year','average diesel price',
-                                       'average gasoline price', 'household size',
-                                       'household size year', 'congestion rank',
-                                       'peak hours spent in congestion', 'congestion index',
-                                       'average congestion rate', 'congestion level', 
-                                       'congestion change', 'morning peak', 'evening peak', 
-                                       'economy rank', 'environment rank', 'governance rank',
-                                       'human capital rank', 'international impact rank', 
-                                       'mobility and transportation rank', 'public management rank', 
-                                       'social cohesion rank', 'technology rank', 
-                                       'urban planning rank', 'CIMI', 'CIMI Ranking', 
-                                       'CIMI Performance', 'exports', 'production', 
-                                       'natgas exports', 'C4H waste per capita',
-                                       'C4H waste plus natgas per capita', 
-                                       'corrected C4H waste plus natgas per capita', 
-                                       '')
+colnames(correct_dataset_columns) <- c(
+  'scope 1 emissions', 
+  'scope 2 emissions','total emissions',
+  's1 lower bound',
+  's1 upper bound', 's1 mean', 'tot lower bound',
+  'tot upper bound', 'tot mean',
+  'co2 emissions per capita', 'region', 'population',
+  'city area km',
+  'ngdp',
+  'energy per person co2', 'center of commerce index',
+  'mean one-way travel time',
+  'congestion rank',
+  'social cohesion rank', 'technology rank', 
+  '')
+
+
+# Prints the correlation between 'total emissions' and all other columns
+# between 0.3 and 0.5 is considered 'moderately correlated. Between 0.5 and 0.7
+# is highly correlated, and higher than 0.7 is considered highly correlated.
+correlations <- cor(correct_dataset_columns$`total emissions`, "storage.mode<-"(as.matrix(correct_dataset_columns[c(1:ncol(correct_dataset_columns))]), "numeric"), use = "pairwise.complete.obs")
+format(correlations)  
+
 
 #-----------------
 #DATA VISUALIZATION
@@ -76,21 +56,14 @@ ggplot(data = correct_dataset_columns) +
   geom_point(mapping = aes(x = population, y = `tot mean`, color=region))
 
 # Graph average gas price vs. emissions
- ggplot(data = correct_dataset_columns) +
-   geom_point(mapping = aes(x = `average gasoline price`, y = `tot mean`, color=region)) +
-   coord_cartesian(ylim=c(0, 30000000))
+ggplot(data = correct_dataset_columns) +
+  geom_point(mapping = aes(x = `average gasoline price`, y = `tot mean`, color=region))
 
 # Graph median income vs. emissions
- ggplot(data = correct_dataset_columns) +
-   geom_smooth(mapping = aes(x = `ngdp per capita`, y = `tot mean`, color=region))
+ggplot(data = correct_dataset_columns) +
+  geom_smooth(mapping = aes(x = `ngdp per capita`, y = `tot mean`, color=region))
 
 # Graph annual mean temperature vs emissions
- ggplot(data = correct_dataset_columns) +
-   geom_point(mapping = aes(x = `average annual temperature celcius`, y = `tot mean`, color=region))
- 
-# Example multinomial logistic regression model
- xH <- model.matrix(`total emissions` ~ population , data=correct_dataset_columns)
- yH <- correct_dataset_columns$`total emissions`
- netfit <- glmnet(xH, yH, family='multinomial')
- lmin <- min(netfit$lambda)
- pnet <- drop(predict(netfit, xH, s=lmin, type="response"))
+ggplot(data = correct_dataset_columns) +
+  geom_point(mapping = aes(x = `average annual temperature celcius`, y = `tot mean`, color=region))
+
